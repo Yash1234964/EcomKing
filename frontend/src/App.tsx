06aa52af54fbe61f826c0c106f828d157bb7a85c
Yash Raj <ryash8500@gmail.com>
2023-08-04 16:57:51 +0530

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,27 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div>
+      <header>
+           fdsdfsdf
+      </header>
+      <main>
+
+      </main>
+      <footer>
+             fdsfdsfsdf
+      </footer>
+    </div>
+    </>
+  )
+}
+
+export default App
