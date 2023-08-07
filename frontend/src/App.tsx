6119a7dbc262da5eb44293d3e31004eddc1811d7
Yash Raj <ryash8500@gmail.com>
2023-08-07 11:04:51 +0530

    Applied light and dark mode feature

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,34 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { Navbar,Container,NavbarBrand,Nav,Row,Col } from 'react-bootstrap'
+import { Navbar,Container,NavbarBrand,Nav,Row,Col, Button } from 'react-bootstrap'
 import { sampleProducts } from './data'
 import { Outlet } from 'react-router-dom'
+import { useContext, useEffect } from 'react';
+import { Store } from './store';
 
 function App() {
+
+  const {state:{mode} , dispatch} = useContext(Store);
+
+  useEffect(() => {
+     document.body.setAttribute('data-bs-theme',mode)
+  },[mode])
+
+  const switchModeHandler = () => {
+    dispatch({type:'SWITCH_MODE'})
+  }
 
   return (
     <>
     <div className='d-flex flex-column h-full vh-100'>
       <header>
-        <Navbar bg="dark" variant="dark" expand="lg">
+        <Navbar  expand="lg">
           <Container>
           <NavbarBrand>Ecom King</NavbarBrand>
           </Container>
           <Nav>
+            <Button variant={mode} onClick = {switchModeHandler} >
+              <i className={mode=== 'light'?'fa fa-sun' : 'fa fa-moon'} > </i>
+            </Button>
             <a href='/cart' className="nav-link">Cart</a>
             <a href='/login' className="nav-link">Sign In</a>
           </Nav>
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -16,6 +16,7 @@ import ProductPage from './pages/ProductPage.tsx'
 import axios from 'axios'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { StoreProvider } from './store.tsx'
 
 
 
@@ -34,11 +35,13 @@ const queryClient= new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <StoreProvider>
     <HelmetProvider>
     <QueryClientProvider client = {queryClient}>
     <RouterProvider router ={router}></RouterProvider>
     <ReactQueryDevtools initialIsOpen={false}></ReactQueryDevtools>
     </QueryClientProvider>
     </HelmetProvider>
+    </StoreProvider>
   </React.StrictMode>,
 )
