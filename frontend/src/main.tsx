ec52444ae9b4ff7320eca9b1cb05b18da3c540ab
Yash Raj <ryash8500@gmail.com>
2023-08-08 16:30:50 +0530

    Cart page implemented

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -16,7 +16,8 @@ import ProductPage from './pages/ProductPage.tsx'
 import axios from 'axios'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import { StoreProvider } from './store.tsx'
+import { StoreProvider } from './Store.tsx'
+import CartPage from './pages/CartPage.tsx'
 
 
 
@@ -25,6 +26,7 @@ const router = createBrowserRouter(
     <Route path="/" element={<App />}>
       <Route index={true} element={<HomePage/>}/>
       <Route path="product/:slug" element={<ProductPage />} />
+      <Route path="cart" element={<CartPage/>} />
       {/* <Route path="dashboard" element={<Dashboard />} /> */}
       {/* ... etc. */}
     </Route>
