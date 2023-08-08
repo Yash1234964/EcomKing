47d99ab10b84099eced9fc4e16cba6eec340c968
Yash Raj <ryash8500@gmail.com>
2023-08-08 16:50:32 +0530

    Implemented delete button on cart page

--- frontend/src/pages/CartPage.tsx
+++ frontend/src/pages/CartPage.tsx
@@ -32,6 +32,10 @@ export default function CartPage() {
     navigate('/signin?redirect=/shipping')
   }
 
+  const removeItemHandler =(item : CartItem) =>{
+    dispatch({type:'CART_REMOVE_ITEM' , payload:item})
+  }
+
   return (
     <div>
       <Helmet>
@@ -80,7 +84,7 @@ export default function CartPage() {
                     </Col>
                     <Col md={3}>₹{item.price}</Col>
                     <Col md={2}>
-                      <Button variant={mode}>
+                      <Button variant={mode} onClick={()=>removeItemHandler(item)}>
                         <i className="fas fa-trash"></i>
                       </Button>
                     </Col>
