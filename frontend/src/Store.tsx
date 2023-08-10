19780f53f91f70b607860f400676a70973bd4072
Yash Raj <ryash8500@gmail.com>
2023-08-10 07:59:45 +0530

    Shipping Address page implemented

--- frontend/src/Store.tsx
+++ frontend/src/Store.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-case-declarations */
 import React from "react";
-import { Cart, CartItem } from "./types/Cart";
+import { Cart, CartItem, ShippingAddress } from "./types/Cart";
 import { UserInfo } from "./types/UserInfo";
 
 type AppState = {
@@ -43,7 +43,8 @@ type Action =
   | { type: "CART_ADD_ITEM"; payload: CartItem }
   | { type: "CART_REMOVE_ITEM"; payload: CartItem }
   | { type: "USER_SIGNIN"; payload: UserInfo }
-  | { type: "USER_SIGNOUT" };
+  | { type: "USER_SIGNOUT" }
+  | { type: "SAVE_SHIPPING_ADDRESS"; payload: ShippingAddress };
 
 function reducer(state: AppState, action: Action): AppState {
   switch (action.type) {
@@ -96,6 +97,15 @@ function reducer(state: AppState, action: Action): AppState {
           shippingPrice: 0,
           taxPrice: 0,
           totalPrice: 0,
+        },
+      };
+
+    case "SAVE_SHIPPING_ADDRESS":
+      return {
+        ...state,
+        cart: {
+          ...state.cart,
+          shippingAddress: action.payload,
         },
       };
 
